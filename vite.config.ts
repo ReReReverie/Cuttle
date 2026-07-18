0d36eb884af68b3a2a45042b8909711580209deb
Lanz Martene <lanzmartene10@gmail.com>
2026-07-18 13:02:05 +0800

    unfulfilled

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { execSync } from 'child_process'
 import url from 'url'
 
@@ -72,6 +73,6 @@ function gitPlugin() {
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), gitPlugin()],
+  plugins: [tailwindcss(), react(), gitPlugin()],
 })
 
